6698fa1b7fdc692b87a6f7160e91c92c070f8f1c
mikeusry <mikeusry@gmail.com>
2012-10-21 05:17:07 -0700

    7.16

--- soil_conditioner/js/js_6wzOnaApmG_hb1WYGP-HGUMVjPyhNPkExUxwCj22PLg.js
+++
@@ -1,9 +0,0 @@
-// user identification
-  _kmq.push(['identify', '$user->name']);
-
-// user identification
-  _kmq.push(['record', 'My Event']);
-
-// user identification
-  _kmq.push(['record', 'My Event', {'My Property':'Value'}]);
-;
